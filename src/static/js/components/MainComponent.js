8c656d06a553e8c7ed5e8fc8890e92ebebf263e0
gamblinflanagan <gamblinflanagan97@gmail.com>
2021-08-01 13:14:00 -0400

    change style of image on home page, change path to tab icon

--- src/static/js/components/MainComponent.js
+++ src/static/js/components/MainComponent.js
@@ -155,7 +155,7 @@ const MainComponent = () => {
                             <div className="Content">
                                 <img id="profile" src={profile} />
                                 <p>
-                                    I am Joey Flanagan. I am a full stack developer and NJIT alumni.
+                                    I am Joey Flanagan. I am an NJIT alumni and currently an Automation Test Engineer at IBM.
                                     More About Me in the <Link id="more" to='/about'>Resume Page</Link>
                                 </p>
                             </div> 
